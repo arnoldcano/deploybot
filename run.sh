46ac9703ab0929b4f1206a09467ad02030de2c39
Arnold Cano <arnoldcano@yahoo.com>
2016-10-12 20:34:01 -0500

    more template tweaks and run command update

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-if [ -z "$HUBOT_SLACK_TOKEN" ]; then
-  echo "HUBOT_SLACK_TOKEN is not set"
-  exit 1
-fi
-
-if [ -z "$HUBOT_SEMAPHOREAPP_DEPLOY" ]; then
-  echo "HUBOT_SEMAPHOREAPP_DEPLOY is not set"
+if [ -z "$HUBOT_AUTH_ADMIN" ]; then
+  echo "HUBOT_AUTH_ADMIN is not set"
   exit 1
 fi
 
@@ -15,8 +10,28 @@ if [ -z "$HUBOT_SEMAPHOREAPP_AUTH_TOKEN" ]; then
   exit 1
 fi
 
-if [ -z "$HUBOT_AUTH_ADMIN" ]; then
-  echo "HUBOT_AUTH_ADMIN is not set"
+if [ -z "$HUBOT_SEMAPHOREAPP_DEPLOY" ]; then
+  echo "HUBOT_SEMAPHOREAPP_DEPLOY is not set"
+  exit 1
+fi
+
+if [ -z "$HUBOT_SEMAPHOREAPP_NOTIFY_RULES" ]; then
+  echo "HUBOT_SEMAPHOREAPP_NOTIFY_RULES is not set"
+  exit 1
+fi
+
+if [ -z "$HUBOT_SEMAPHOREAPP_TRIGGER" ]; then
+  echo "HUBOT_SEMAPHOREAPP_TRIGGER is not set"
+  exit 1
+fi
+
+if [ -z "$HUBOT_SLACK_TOKEN" ]; then
+  echo "HUBOT_SLACK_TOKEN is not set"
+  exit 1
+fi
+
+if [ -z "$REDIS_URL" ]; then
+  echo "REDIS_URL is not set"
   exit 1
 fi
 
