2e13b2363fc6af100f946f86e63267ba5a5e9da6
Arnold Cano <arnoldcano@yahoo.com>
2016-11-14 11:56:43 -0600

    small fixes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,26 +6,26 @@ TAG=`git rev-parse --short HEAD`
 DCOS_CLI_URL='https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.8/dcos'
 
 check_environment() {
-  if [ -z "$DCOS_URL" ]; then
+  if [ -z $DCOS_URL ]; then
     echo 'DCOS_URL is not set'
     exit 1
   fi
-  if [ -z "$DCOS_ACS_TOKEN" ]; then
+  if [ -z $DCOS_ACS_TOKEN ]; then
     echo 'DCOS_ACS_TOKEN is not set'
     exit 1
   fi
-  if [ -z "$HUBOT_SEMAPHOREAPP_AUTH_TOKEN" ]; then
+  if [ -z $HUBOT_SEMAPHOREAPP_AUTH_TOKEN ]; then
     echo 'HUBOT_SEMAPHOREAPP_AUTH_TOKEN is not set'
     exit 1
   fi
-  if [ -z "$HUBOT_SLACK_TOKEN" ]; then
+  if [ -z $HUBOT_SLACK_TOKEN ]; then
     echo 'HUBOT_SLACK_TOKEN is not set'
     exit 1
   fi
 }
 
 setup_dcos_cli() {
-  if ! type dcos > /dev/null; then
+  if ! which dcos > /dev/null; then
     curl -fLsS --retry 20 -Y 100000 -y 60 $DCOS_CLI_URL -o dcos
     sudo mv dcos /usr/local/bin
     sudo chmod +x /usr/local/bin/dcos
@@ -35,7 +35,7 @@ setup_dcos_cli() {
 }
 
 prepare_deploy() {
-  if [ ! -f "$TEMPLATE" ]; then
+  if [ ! -f $TEMPLATE ]; then
     echo "$TEMPLATE not found"
     exit 1
   fi
@@ -47,10 +47,10 @@ prepare_deploy() {
 }
 
 deploy_to_dcos() {
-  if dcos marathon app show $NAME 2>&1 | grep -q 'Error'; then
-    dcos marathon app add < $TAG.json
-  else
+  if dcos marathon app list | grep -q $NAME; then
     dcos marathon app update $NAME < $TAG.json
+  else
+    dcos marathon app add < $TAG.json
   fi
 }
 
